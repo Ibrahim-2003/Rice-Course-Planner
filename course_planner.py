c7d7861f6d617681422d1458f07f9de899a4c5d6
Ibrahim <ialakash@live.com>
2022-01-20 23:11:00 -0600

    Added my current courses as objects in Course clas

--- course_planner.py
+++ course_planner.py
@@ -81,6 +81,7 @@ class Course():
         self.distribution = distribution
         self.description = description
         self.taken = False
+        self.taking = False
         Course.all_courses.append(self)
 
     def tookCourse(self, grade):
@@ -93,12 +94,16 @@ class Course():
         Course.gpa = Course.quality_points / Course.hours
     
     def takingCourse(self):
-        self.taken = True
+        self.taking = True
 
     def getInfo(self):
         print(f'Name: {self.name}\nDepartment: {self.department}\nHours: {self.hours}\nDistribution: {self.distribution}\nDescription: {self.description}')
         return
 
+def getCourse(code):
+    for course in Course.all_courses:
+        if course.course_code == code:
+            return course
 
 Course(course_code="PHYS 101", name="MECHANICS", hours=4, distribution=3, description="A calculus-based introduction to mechanics. Includes classes and lab exercises on kinematics, Newton's Laws, work and energy, conservation laws and rotational motion. Primarily for physical science and engineering students.")
 Course(course_code="MATH 211", name="ORDINARY DIFFERENTIAL EQUATIONS AND LINEAR ALGEBRA", hours=3, description="Study of ordinary differential equations (e.g., solutions to separable and linear first-order equations and to higher-order linear equations with constant coefficients, the properties of solutions to differential equations, and numerical solution methods) and linear algebra (e.g., vector spaces and solutions to algebraic linear equations, dimension, eigenvalues, and eigenvectors of a matrix), as well as the application of linear algebra to first-order systems of differential equations and the qualitative theory of nonlinear systems and phase portraits.")
@@ -110,3 +115,15 @@ for course in Course.all_courses:
         course.tookCourse("A+")
 
 s22 = ["CAAM 210", "BIOS 201", "CHEM 122", "MATH 212", "PHYS 100", "PHYS 116", "FWIS 188"]
+Course(course_code="CAAM 210", name="INTRODUCTION TO ENGINEERING COMPUTATION", hours=3, description="Modeling, Simulation, and Visualization via MATLAB. Numerical methods: Newton's method in one and several dimensions. Gaussian elimination and optimization. Application to problems in science and engineering.")
+Course(course_code="BIOS 201", name="INTRODUCTORY BIOLOGY I", hours=3, distribution=3, description="This course examines chemistry and energetics, cell physiology, cell biology, Mendelian genetics, molecular genetics, developmental biology, and plant physiology.")
+Course(course_code="CHEM 122", name="GENERAL CHEMISTRY II", hours=4, distribution=3, description="Introduction of chemical phenomena emphasizing problems and methods in Chemistry.")
+Course(course_code="MATH 212", name="MULTIVARIABLE CALCULUS", hours=3, description="Calculus of multiple variables. Vectors, partial derivatives and gradients, double and triple integrals, vector fields, line and surface integrals, Green's theorem, Stokes's theorem, and Gauss's theorem.")
+Course(course_code="PHYS 100", name="EXPLORING PHYSICS", hours=1, description="Introduction to concepts, methods, debates, and discoveries of physics, with a theme to be chosen from one of many fields of modern physics research.")
+Course(course_code="PHYS 116", name="SEMINAR IN PHYSICS AND ASTRONOMY AT RICE AND BEYOND", hours=1, description="This half-semester seminar course will meet in the first half of the Spring semester to introduce prospective and current science and engineering majors to the exciting research in physics and astronomy at Rice and beyond. The course will provide students with the context to think about how the facts presented in physics and astronomy textbooks are applied to real-world research. Undergraduate students in a small group will meet weekly with a graduate student to explore a published research article by a local lab, learning about what was done and why it was important. Toward the end of the course, the group will tour the lab that produced the featured article.")
+Course(course_code="FWIS 188", name="ENGINEERING DESIGN AND COMMUNICATION", hours=3, description="Students learn the engineering design process to solve real-world problems by evaluating design requirements and constructing innovative solutions in the OEDK. Several communication assignments will be completed by individuals rather than teams.", cross=["ENGI 120", "ENGI 220"])
+
+for course in Course.all_courses:
+    if course.course_code in s22:
+        course.takingCourse()
+
